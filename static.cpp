fd502a38407a0f2aed8129108e0dc4b9121101b3
19A20Y1 <97735169+19A20Y1@users.noreply.github.com>
2023-07-01 11:27:29 +0545

    Add files via upload

---
+++ static.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+class test{
+    int code;
+    static int count;
+    public:
+    void setcode(){
+        code=++count;
+    }
+    void showcode(){
+        cout<<"Object number: "<<code<<endl;
+    }
+    static void showcount(){
+        cout<<"Count: "<<count<<endl;
+    }
+};
+int test::count;
+int main(){
+    test t1,t2;
+    t1.setcode();
+    t2.setcode();
+    t1.showcount();
+    t2.showcount();
+    test::showcount();
+    test t3;
+    t3.test::setcode();
+    test::showcount();
+    t1.showcode();
+    t2.showcode();
+    t3.showcode();
+    return 0;
+}
